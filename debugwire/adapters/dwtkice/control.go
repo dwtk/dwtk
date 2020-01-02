e59ccb4eb2a5686cd96371c82c975119d8f925fa
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2020-01-02 17:51:45 +0100

    debugwire: dwtk-ice: sync with newest firmware

--- debugwire/adapters/dwtkice/control.go
+++ debugwire/adapters/dwtkice/control.go
@@ -9,12 +9,23 @@ import (
 
 var (
 	iceErrors = map[uint8]error{
-		1: errors.New("debugwire: dwtk-ice: hardware not initialized"),
-		2: errors.New("debugwire: dwtk-ice: baudrate detection failed"),
-		3: errors.New("debugwire: dwtk-ice: got unexpected byte echoed back"),
-		4: errors.New("debugwire: dwtk-ice: got unexpected break value"),
+		1: errors.New("debugwire: dwtk-ice: baudrate detection failed"),
+		2: errors.New("debugwire: dwtk-ice: got unexpected byte echoed back"),
+		3: errors.New("debugwire: dwtk-ice: got unexpected break value"),
+		4: errors.New("debugwire: dwtk-ice: read/write data is too large"),
 	}
 )
+
+func (dw *DwtkIceAdapter) codeToError(e byte) error {
+	if e == 0 {
+		return nil
+	}
+	err, ok := iceErrors[e]
+	if !ok {
+		return fmt.Errorf("debugwire: dwtk-ice: unrecognized hardware error: 0x%02x", e)
+	}
+	return err
+}
 
 func (dw *DwtkIceAdapter) controlGetError() error {
 	f := make([]byte, 1)
@@ -22,14 +33,7 @@ func (dw *DwtkIceAdapter) controlGetError() error {
 		return err
 	}
 	logger.Debug.Printf("<<< cmdGetError: 0x%02x", f[0])
-	if f[0] == 0 {
-		return nil
-	}
-	err, ok := iceErrors[f[0]]
-	if !ok {
-		return fmt.Errorf("debugwire: dwtk-ice: unrecognized hardware error: 0x%02x", f[0])
-	}
-	return err
+	return dw.codeToError(f[0])
 }
 
 func (dw *DwtkIceAdapter) controlIn(req byte, val uint16, idx uint16, data []byte) error {
@@ -39,13 +43,16 @@ func (dw *DwtkIceAdapter) controlIn(req byte, val uint16, idx uint16, data []byt
 	} else {
 		logger.Debug.Printf("<<< %d(0x%04x, 0x%04x)", req, val, idx)
 	}
-	if err := dw.device.ControlIn(req, val, idx, data); err != nil {
+	f := make([]byte, len(data)+1)
+	if err := dw.device.ControlIn(req, val, idx, f); err != nil {
 		return err
 	}
-	for _, d := range data {
+	logger.Debug.Printf("<<< error: 0x%02x", f[0])
+	for i, d := range f[1:] {
+		data[i] = d
 		logger.Debug.Printf("<<< 0x%02x", d)
 	}
-	return dw.controlGetError()
+	return dw.codeToError(f[0])
 }
 
 func (dw *DwtkIceAdapter) controlOut(req byte, val uint16, idx uint16, data []byte) error {
