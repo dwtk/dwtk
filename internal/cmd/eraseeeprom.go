1ad144b416f1890c23f63101d7330e60f89f4fa7
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2020-04-04 01:33:07 +0200

    internal/cmd: added command to erase eeprom

---
+++ internal/cmd/eraseeeprom.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(EraseEEPROMCmd)
+}
+
+var EraseEEPROMCmd = &cobra.Command{
+	Use:   "erase-eeprom",
+	Short: "erase target MCU's EEPROM and exit",
+	Long:  "This command erases target MCU's EEPROM and exits.",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		f := make([]byte, dw.MCU.EEPROMSize)
+		for i := uint16(0); i < dw.MCU.EEPROMSize; i++ {
+			f[i] = 0xff
+		}
+
+		cmd.Printf("Erasing 0x%04x bytes from EEPROM ...\n", dw.MCU.EEPROMSize)
+		return dw.WriteEEPROM(0, f)
+	},
+}
